42e046601f3e696d287f33cb5d691e8907a7cd00
phamHoc <phamhoc2902@gmail.com>
2023-05-17 21:41:31 +0700

    layout home IG

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+import Instagram from './src/component/ig';
+AppRegistry.registerComponent(appName, () => Instagram);
